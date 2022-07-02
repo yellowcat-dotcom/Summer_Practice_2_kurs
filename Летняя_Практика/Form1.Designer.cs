f531a2b708d78e3ee66ed65c3e10555bfa9f325c
Валентина Гончаренко <valentinagoncarenko975@gmail.com>
2022-07-02 15:04:33 +0300

    Добавьте файлы проекта.

---
+++ Летняя_Практика/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace Летняя_Практика
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EM = new System.Windows.Forms.Button();
+            this.ClearAll = new System.Windows.Forms.Button();
+            this.iter = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // EM
+            // 
+            this.EM.BackColor = System.Drawing.Color.White;
+            this.EM.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.EM.ForeColor = System.Drawing.Color.Maroon;
+            this.EM.Location = new System.Drawing.Point(740, 206);
+            this.EM.Name = "EM";
+            this.EM.Size = new System.Drawing.Size(205, 93);
+            this.EM.TabIndex = 0;
+            this.EM.Text = "Метод Эйлера";
+            this.EM.UseVisualStyleBackColor = false;
+            this.EM.Click += new System.EventHandler(this.BTN_EM);
+            // 
+            // ClearAll
+            // 
+            this.ClearAll.BackColor = System.Drawing.Color.White;
+            this.ClearAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ClearAll.ForeColor = System.Drawing.Color.Maroon;
+            this.ClearAll.Location = new System.Drawing.Point(740, 343);
+            this.ClearAll.Name = "ClearAll";
+            this.ClearAll.Size = new System.Drawing.Size(205, 93);
+            this.ClearAll.TabIndex = 8;
+            this.ClearAll.Text = "Очистить график";
+            this.ClearAll.UseVisualStyleBackColor = false;
+            this.ClearAll.Click += new System.EventHandler(this.BTN_Clear);
+            // 
+            // iter
+            // 
+            this.iter.BackColor = System.Drawing.Color.White;
+            this.iter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.iter.ForeColor = System.Drawing.Color.Maroon;
+            this.iter.Location = new System.Drawing.Point(740, 140);
+            this.iter.Name = "iter";
+            this.iter.Size = new System.Drawing.Size(205, 27);
+            this.iter.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(740, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 93);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Введите количество итераций";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Maroon;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(740, 473);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 93);
+            this.label1.TabIndex = 17;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(977, 749);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.iter);
+            this.Controls.Add(this.ClearAll);
+            this.Controls.Add(this.EM);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.Maroon;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Летняя практика";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button EM;
+        private System.Windows.Forms.Button ClearAll;
+        private System.Windows.Forms.TextBox iter;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox h;
+        private System.Windows.Forms.Label curStep;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Летняя_Практика/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Летняя_Практика
+{
+    public partial class Form1 : Form
+    {        
+        public string ME = "Метод Эйлера";
+        public string ME_1 = "Точное решение";
+        ZedGraphControl zedGrapgControl1 = new ZedGraphControl();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            zedGrapgControl1.Location = new Point(10, 30);
+            zedGrapgControl1.Name = "text";
+            zedGrapgControl1.Size = new Size(700, 700);
+            Controls.Add(zedGrapgControl1);
+            GraphPane my_Pane = zedGrapgControl1.GraphPane;
+            my_Pane.Title.Text = "График функции";
+            my_Pane.XAxis.Title.Text = " X";
+            my_Pane.YAxis.Title.Text = " Y";
+        }
+        private void GetSize()
+        {
+            zedGrapgControl1.Location = new Point(10, 10);
+            zedGrapgControl1.Size = new Size(ClientRectangle.Width - 20, ClientRectangle.Height - 20);
+        }
+        static double f(double x)
+        {
+            return Math.Sqrt(Math.Sin(x)+Math.Exp(-1*Math.Sin(x))); //решение вручную
+        }
+        static double f1(double x, double y) 
+        {
+            return ((((Math.Cos(x)*(1+ Math.Sin(x)))/y)-(y* Math.Cos(x)))/2);
+        }     
+        private void Eiler(ZedGraphControl Zed_GraphControl)
+        {
+            GraphPane my_Pane = Zed_GraphControl.GraphPane;
+            PointPairList list = new PointPairList();
+            PointPairList list_1 = new PointPairList();
+
+            double m = -1;
+
+            double y = 1; 
+            double x = 0;
+
+            double a = 0;
+
+            double n = Convert.ToDouble(iter.Text);
+            double h = 1/n;
+
+            for (double i = 0; i <= n; i++)
+            {
+                list_1.Add(a + h * i, f(a + h * i));
+
+                list.Add(x, y);
+                y = y + h * f1(x, y);
+                x += h;
+
+                if (Math.Abs(y - f(a + h * i)) > m) m = Math.Abs(y - f(a + h * i));
+            }
+            
+            LineItem myCircle_1 = my_Pane.AddCurve(ME_1, list_1, Color.Red, SymbolType.None);
+            LineItem myCircle = my_Pane.AddCurve(ME, list, Color.Blue, SymbolType.None);
+            zedGrapgControl1.AxisChange();
+            zedGrapgControl1.Invalidate();
+            label1.BackColor = Color.White;
+            label1.Text = Convert.ToString("Максимальная невязка:\n "+m);
+        }      
+        private void GriddenOn(GraphPane my_Pane)
+        {
+            my_Pane.XAxis.MajorGrid.IsVisible = true;
+            my_Pane.YAxis.MajorGrid.IsVisible = true;
+            my_Pane.YAxis.MinorGrid.IsVisible = true;
+            my_Pane.XAxis.MinorGrid.IsVisible = true;
+        }
+        private void Clear(ZedGraphControl Zed_GraphControl)
+        {            
+            zedGrapgControl1.GraphPane.CurveList.Clear();
+            zedGrapgControl1.GraphPane.GraphObjList.Clear();
+            zedGrapgControl1.GraphPane.XAxis.Type = AxisType.Linear;
+            zedGrapgControl1.GraphPane.XAxis.Scale.TextLabels = null;
+            zedGrapgControl1.GraphPane.XAxis.MajorGrid.IsVisible = false;
+            zedGrapgControl1.GraphPane.YAxis.MajorGrid.IsVisible = false;
+            zedGrapgControl1.GraphPane.YAxis.MinorGrid.IsVisible = false;
+            zedGrapgControl1.GraphPane.XAxis.MinorGrid.IsVisible = false;
+            zedGrapgControl1.RestoreScale(zedGrapgControl1.GraphPane);
+            zedGrapgControl1.AxisChange();
+            zedGrapgControl1.Invalidate();
+        }
+        private void BTN_EM(object sender, EventArgs e)
+        {
+            GriddenOn(zedGrapgControl1.GraphPane);
+            Eiler(zedGrapgControl1);            
+        }
+        private void BTN_Clear(object sender, EventArgs e)
+        {
+            Clear(zedGrapgControl1);            
+            label1.BackColor = Color.Maroon;
+        }      
+    }
+}
